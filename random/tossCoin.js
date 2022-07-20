e869df5fd03dae833692c3222204f7f7127fc8e9
Shaurya-Sehgal <ashish.shaurya.as@gmail.com>
2022-07-20 17:03:03 -0600

    Added more tools

---
+++ random/tossCoin.js
@@ -0,0 +1,9 @@
+function check() {
+  let num = Math.random() * 100;
+  num = Math.floor(num);
+  if (num % 2 == 0) {
+    document.getElementById("result").innerText = "Heads!";
+  } else {
+    document.getElementById("result").innerText = "Tails!";
+  }
+}
