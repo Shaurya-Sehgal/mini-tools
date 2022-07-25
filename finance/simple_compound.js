1ec22c37a3c8a7ce9e01aafec83575352e8dd622
Shaurya-Sehgal <ashish.shaurya.as@gmail.com>
2022-07-25 17:02:42 -0600

    Added tool

---
+++ finance/simple_compound.js
@@ -0,0 +1,14 @@
+function check() {
+  let interest;
+  let amount = document.getElementById("amount").value;
+  let rate = document.getElementById("rate").value;
+  let time = document.getElementById("time").value;
+  let select = document.getElementById("options");
+  let value = select.options[select.selectedIndex].value;
+  if (value == "simple") {
+    interest = amount * (rate / 100) * time;
+  } else {
+    interest = amount * (1 + rate / 100) ** time;
+  }
+  document.getElementById("result").innerText = "Your Interest is " + interest;
+}
