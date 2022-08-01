258c0f72b7a194195ea41f95e1b3340afe65175e
Shaurya-Sehgal <ashish.shaurya.as@gmail.com>
2022-08-01 15:01:19 -0600

    Added tool

---
+++ finance/tax.js
@@ -0,0 +1,16 @@
+function check() {
+  let income = document.getElementById("income").value;
+  let tax = null;
+  if (income <= 50197) {
+    tax = income * 0.15;
+  } else if (income <= 100392) {
+    tax = income * 0.205;
+  } else if (income <= 155625) {
+    tax = income * 0.26;
+  } else if (income <= 221708) {
+    tax = income * 0.2938;
+  } else {
+    tax = income * 0.33;
+  }
+  document.getElementById("result").innerText = "Your tax is $" + tax;
+}
