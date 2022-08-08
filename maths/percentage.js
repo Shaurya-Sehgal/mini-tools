7961f30615388d9d5f52a97dc89b807559f016a5
Shaurya-Sehgal <ashish.shaurya.as@gmail.com>
2022-08-08 16:33:14 -0600

    Made more responsive

--- maths/percentage.js
+++ maths/percentage.js
@@ -4,3 +4,10 @@ function check() {
   let percent = (number / outOf) * 100;
   document.getElementById("result").innerText = percent + "%";
 }
+
+function check2() {
+  let number = document.getElementById("number2").value;
+  let outOf = document.getElementById("outOf2").value;
+  let percent = (number / outOf) * 100;
+  document.getElementById("result").innerText = percent + "%";
+}
